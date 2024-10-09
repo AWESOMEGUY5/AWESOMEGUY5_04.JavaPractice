05e9d7d600a0eb05fd1a20c9d74413187cea4ca1
AWESOMEGUY5 <gusanal123@naver.com>
2024-10-09 17:47:03 +0900

    20241009

--- src/C2_19.java
+++ src/C2_19.java
@@ -7,13 +7,52 @@ public class C2_19 {
         System.out.print("금액을 입력하시오. >>>");
         int money = scanner.nextInt();
 
-        System.out.println("5만원권 " + money / 50000 + "매");
-        System.out.println("1만원권 " + money % 50000 / 10000 + "매");
-        System.out.println("5천원권 " + money % 50000 % 10000 / 5000 + "매");
-        System.out.println("1천원권 " + money % 50000 % 10000 % 5000 / 1000 + "매");
-        System.out.println("500원 " + money % 50000 % 10000 % 5000 % 1000 / 500 + "개");
-        System.out.println("100원 " + money % 50000 % 10000 % 5000 % 1000 % 500 / 100 + "개");
-        System.out.println("50원 " + money % 50000 % 10000 % 5000 % 1000 % 500 % 100 / 50 + "개");
-        System.out.println("10원 " + money % 50000 % 10000 % 5000 % 1000 % 500 % 100 % 50 / 10 + "개");
+        if(money >= 50000) {
+            int count = money / 50000;
+            money %= 50000;
+            System.out.printf("5만원 %d 매\n", count);
+        }
+
+        if(money >= 10000) {
+            int count = money / 10000;
+            money %= 10000;
+            System.out.printf("1만원 %d 매\n", count);
+        }
+
+        if(money >= 5000) {
+            int count = money / 5000;
+            money %= 5000;
+            System.out.printf("5천원 %d 매\n", count);
+        }
+
+        if(money >= 1000) {
+            int count = money / 1000;
+            money %= 1000;
+            System.out.printf("1천원 %d 매\n", count);
+        }
+
+        if(money >= 500) {
+            int count = money / 500;
+            money %= 500;
+            System.out.printf("500원 %d 매\n", count);
+        }
+
+        if(money >= 100) {
+            int count = money / 100;
+            money %= 100;
+            System.out.printf("100원 %d 매\n", count);
+        }
+
+        if(money >= 50) {
+            int count = money / 50;
+            money %= 50;
+            System.out.printf("50원 %d 개\n", count);
+        }
+
+        if(money >= 10) {
+            int count = money / 10;
+            money %= 10;
+            System.out.printf("10원 %d 개\n", count);
+        }
     }
 }
